f47d57a6b3b08f0722633a1c9bd697d326687566
adelamtuduce <adelamtuduce@gmail.com>
2016-06-25 20:33:48 +0300

    Change seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,40 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # create admin for application
-Role.destroy_all
-User.destroy_all
-Student.destroy_all
-Teacher.destroy_all
-Report.destroy_all
-Faculty.destroy_all
-Section.destroy_all
+Conversation.destroy_all
 DiplomaProject.destroy_all
 DiplomaSelection.destroy_all
 Document.destroy_all
-Role.reset_pk_sequence
-User.reset_pk_sequence
-Student.reset_pk_sequence
-Teacher.reset_pk_sequence
-Report.reset_pk_sequence
-Faculty.reset_pk_sequence
-Section.reset_pk_sequence
+EnrollRequest.destroy_all
+Event.destroy_all
+Faculty.destroy_all
+Message.destroy_all
+Notification.destroy_all
+PersonalInformation.destroy_all
+Report.destroy_all
+Role.destroy_all
+Section.destroy_all
+Student.destroy_all
+StudentEvent.destroy_all
+Teacher.destroy_all
+User.destroy_all
+Conversation.reset_pk_sequence
 DiplomaProject.reset_pk_sequence
 DiplomaSelection.reset_pk_sequence
 Document.reset_pk_sequence
+EnrollRequest.reset_pk_sequence
+Event.reset_pk_sequence
+Faculty.reset_pk_sequence
+Message.reset_pk_sequence
+Notification.reset_pk_sequence
+PersonalInformation.reset_pk_sequence
+Report.reset_pk_sequence
+Role.reset_pk_sequence
+Section.reset_pk_sequence
+Student.reset_pk_sequence
+StudentEvent.reset_pk_sequence
+Teacher.reset_pk_sequence
+User.reset_pk_sequence
 role = Role.new(name: 'admin')
 role.save
 User.create(email: 'admin@example.com', approved: true, password: 'Admin@pass', password_confirmation: 'Admin@pass', role_id: role.id)
